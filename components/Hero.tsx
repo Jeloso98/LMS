e6f0d8e9dff28545871291d00a06913fa8966ad7
Jeloso98 <jaosorio261@soy.sena.edu.co>
2025-05-22 19:14:08 -0500

    feat add component Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+import { Button } from "./ui/button";
+
+
+export default function Hero() {
+    return (
+        <section className="container flex flex-col items-center justify-center gap-6 py-24 text-center md:py-32">
+            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+                Conviertete en <br />
+                un desarrollador{"  "}
+                <span className="bg-gradient bg-gradient-to-r from-blue-500 to-violet-500 bg-clip-text text-transparent">
+                    Full Stack
+                </span>
+                <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
+                    Aprende todo lo relacionado con el desarrollo de software y sus nuevas tecnologías
+                </p>
+                <div className="flex flex-col pt-5 sm:flex-row">
+                    <Button 
+                        variant="default" className="bg-gradient-to-r from-blue-500 to-violet-500 text-white">
+                        Acceder a los cursos
+                    </Button>
+                    
+                </div>
+            </h1>
+        </section>
+    )
+}
